8b337ce98aacc6b23f77cc4ebf532952e5239412
Mark Clements <gitnub@gmail.com>
2022-10-22 19:42:15 +0200

    Add `say_hello`; update index

--- nbdev2_test/_modidx.py
+++ nbdev2_test/_modidx.py
@@ -5,4 +5,5 @@ d = { 'settings': { 'branch': 'master',
                 'doc_host': 'https://mark-clements.github.io',
                 'git_url': 'https://github.com/mark-clements/nbdev2-test',
                 'lib_path': 'nbdev2_test'},
-  'syms': {'nbdev2_test.core': {'nbdev2_test.core.foo': ('core.html#foo', 'nbdev2_test/core.py')}}}
+  'syms': { 'nbdev2_test.core': { 'nbdev2_test.core.foo': ('core.html#foo', 'nbdev2_test/core.py'),
+                                  'nbdev2_test.core.say_hello': ('core.html#say_hello', 'nbdev2_test/core.py')}}}
